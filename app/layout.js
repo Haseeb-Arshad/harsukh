5c3a060e3a00a0345872b7a0bc4a895d7257783e
Haseeb Arshad <Haseebarshad992@gmail.com>
2024-08-29 13:05:31 +0500

    Updated with Facebook Ads Tracking

--- app/layout.js
+++ app/layout.js
@@ -40,8 +40,31 @@ export default function RootLayout({ children }) {
               gtag('config', 'AW-16682968635');
             `}
           </Script>
+          <Script id="facebook-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '7384856538283539');
+              fbq('track', 'PageView');
+            `}
+          </Script>
         </head>
         <body className={inter.className}>
+          <noscript>
+            <img 
+              height="1" 
+              width="1" 
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=7384856538283539&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
           <GoogleTagManager />
           {children}
         </body>
