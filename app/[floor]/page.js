b12cb11ee2d52505ea3676fad7c1f7a7e9b91fdb
Haseeb Arshad <Haseebarshad992@gmail.com>
2024-09-16 17:09:25 +0500

    Added

--- app/[floor]/page.js
+++ app/[floor]/page.js
@@ -13,15 +13,15 @@ const Floor = dynamic(() => import('../component/modules/FloorLayout/floor'), {
 });
 
 const floorData = {
-  "valley-floor-1": { imageLink: "https://cdn.theharsukh.com/floors/floors/basement1.webp", imageName: "valley-floor-1", svgFile: "/svg/basement1/B1.1-01.svg" },
-  "valley-floor-3": { imageLink: "https://cdn.theharsukh.com/floors/floors/basement3.webp", imageName: "valley-floor-3", svgFile: "/svg/basement3/B3-01.svg" },
-  "valley-floor-4": { imageLink: "https://cdn.theharsukh.com/floors/floors/basement4.webp", imageName: "valley-floor-4", svgFile: "/svg/basement4/B4-01.svg" },
-  "valley-floor-5": { imageLink: "https://cdn.theharsukh.com/floors/floors/basement5.webp", imageName: "valley-floor-5", svgFile: "/svg/basement5/B5-01.svg" },
-  "valley-floor-6": { imageLink: "https://cdn.theharsukh.com/floors/floors/basement6.webp", imageName: "valley-floor-6", svgFile: "/svg/basement6/B6-01.svg" },
-  "ground-floor": { imageLink: "https://cdn.theharsukh.com/floors/floors/groundfloor.webp", imageName: "ground-floor", svgFile: "/svg/groundfloor/G-01.svg" },
-  "first-floor": { imageLink: "https://cdn.theharsukh.com/floors/floors/firstfloor.webp", imageName: "first-floor", svgFile: "/svg/firstfloor/F1-01.svg" },
-  "second-floor": { imageLink: "https://cdn.theharsukh.com/floors/floors/secondfloor.webp", imageName: "second-floor", svgFile: "/svg/secondfloor/F2-01.svg" },
-  'third-floor': { imageLink: "https://cdn.theharsukh.com/floors/floors/thirdfloor.webp", imageName: "third-floor", svgFile: "/svg/thirdfloor/F3-01.svg" },
+  "valley-floor-1": { imageLink: "/Webpage/floors/basement1.webp", imageName: "valley-floor-1", svgFile: "/svg/basement1/B1.1-01.svg" },
+  "valley-floor-3": { imageLink: "/Webpage/floors/basement3.webp", imageName: "valley-floor-3", svgFile: "/svg/basement3/B3-01.svg" },
+  "valley-floor-4": { imageLink: "/Webpage/floors/basement4.webp", imageName: "valley-floor-4", svgFile: "/svg/basement4/B4-01.svg" },
+  "valley-floor-5": { imageLink: "/Webpage/floors/basement5.webp", imageName: "valley-floor-5", svgFile: "/svg/basement5/B5-01.svg" },
+  "valley-floor-6": { imageLink: "/Webpage/floors/basement6.webp", imageName: "valley-floor-6", svgFile: "/svg/basement6/B6-01.svg" },
+  "ground-floor": { imageLink: "/Webpage/floors/groundfloor.webp", imageName: "ground-floor", svgFile: "/svg/groundfloor/G-01.svg" },
+  "first-floor": { imageLink: "/Webpage/floors/firstfloor.webp", imageName: "first-floor", svgFile: "/svg/firstfloor/F1-01.svg" },
+  "second-floor": { imageLink: "/Webpage/floors/secondfloor.webp", imageName: "second-floor", svgFile: "/svg/secondfloor/F2-01.svg" },
+  'third-floor': { imageLink: "/Webpage/floors/thirdfloor.webp", imageName: "third-floor", svgFile: "/svg/thirdfloor/F3-01.svg" },
 };
 
 const Page = ({ amenityClicked, updateAmenityClicked }) => {
