11e1c49cced6bc74e110c4de36b7be08f04cb078
Haseeb Arshad <Haseebarshad992@gmail.com>
2024-09-16 12:42:25 +0500

    Image Unoptimize

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-         unoptimized: true,
-  }};
+         unoptimized: false,
+  }
+
+};
   
   export default nextConfig;
