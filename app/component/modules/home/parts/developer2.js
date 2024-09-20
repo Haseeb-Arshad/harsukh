7bb23fe182f7eeeda6eb43e40b5e29b09c309468
Haseeb Arshad <Haseebarshad992@gmail.com>
2024-09-20 18:40:03 +0500

    Fixed issues

--- app/component/modules/home/parts/developer2.js
+++ app/component/modules/home/parts/developer2.js
@@ -9,7 +9,7 @@ const Developer2 = () => {
     <div className={styles.DeveloperPartcontainer}>
        <div className={styles.content}>
             <h2 className={styles.title}>Architect</h2>
-            <Image className={styles.logo} src="/images/icons/Logo/6figures.png" alt="Harsukh Residencies" width={160} height={65} />
+            <Image className={styles.logo} src="https://res.cloudinary.com/dykglphpa/image/upload/v1726839449/harsukh/k9glegpujsehrkikfczn.png" alt="Harsukh Residencies" width={160} height={65} />
             <div className={styles.description}>
                 <p className={styles.desc}>
                     6 Figures Architectural Firm is renowned for their ability to craft spaces that tell a story. With a perfect balance of functionality and artistic expression, their designs are innovative, sustainable, and timeless. Their portfolio includes iconic projects, such as luxury high-rises and peaceful retreats, each reflecting their commitment to excellence. Every structure they create is a testament to their philosophy of blending architecture with the human spirit
