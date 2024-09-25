beea943fdbe7f1d846e266949a592cba59d99ece
Haseeb Arshad <Haseebarshad992@gmail.com>
2024-09-25 12:49:23 +0500

    Robots.txt added

---
+++ app/sitemap.js
@@ -0,0 +1,56 @@
+export default function sitemap() {
+    const today = new Date();
+  
+    return [
+      {
+        url: 'https://theharsukh.com', // Home route
+        lastModified: today,
+        changeFrequency: 'yearly',
+        priority: 1, // Home pages are critical
+      },
+      {
+        url: 'https://theharsukh.com/about',
+        lastModified: today,
+        changeFrequency: 'monthly',
+        priority: 0.8, // "About" pages are important, but rarely updated
+      },
+      {
+        url: 'https://theharsukh.com/blog',
+        lastModified: today,
+        changeFrequency: 'daily',
+        priority: 0.7, // Blog content usually gets updated frequently
+      },
+      {
+        url: 'https://theharsukh.com/callus',
+        lastModified: today,
+        changeFrequency: 'yearly',
+        priority: 0.3, // A "Contact/Call Us" page, so not frequently updated
+      },
+      {
+        url: 'https://theharsukh.com/news-room',
+        lastModified: today,
+        changeFrequency: 'weekly',
+        priority: 0.9, 
+      },
+      {
+        url: 'https://theharsukh.com/map-view',
+        lastModified: today,
+        changeFrequency: 'yearly',
+        priority: 0.3, // A "Contact/Call Us" page, so not frequently updated
+      },
+      {
+        url: 'https://theharsukh.com/explore',
+        lastModified: today,
+        changeFrequency: 'monthly',
+        priority: 0.8, // A "Contact/Call Us" page, so not frequently updated
+      },
+      {
+        url: 'https://theharsukh.com/developer',
+        lastModified: today,
+        changeFrequency: 'monthly',
+        priority: 0.3, // A "Contact/Call Us" page, so not frequently updated
+      },
+
+
+    ]};
+
