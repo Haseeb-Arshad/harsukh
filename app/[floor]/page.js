f340ad4f8d59169cf323608382103d6053e608e7
Haseeb Arshad <Haseebarshad992@gmail.com>
2024-10-03 12:45:54 +0500

    Added MetaTags

--- app/[floor]/page.js
+++ app/[floor]/page.js
@@ -1,7 +1,9 @@
+import { generateMetadata } from './metatag';
 
 import React from 'react'
 import FloorPage from '../component/modules/FloorLayout/floorPage'
 
+export { generateMetadata };
 
 const page = () => {
   return (
