eab7f62eeba1921d8e11a69b22b103f5cf3656b1
Haseeb Arshad <Haseebarshad992@gmail.com>
2024-10-03 12:15:25 +0500

    Added MetaTags

--- app/news-room/page.js
+++ app/news-room/page.js
@@ -1,7 +1,7 @@
-'use client'
+// 'use client'
 
 import React from 'react';
-import BlogPage from '@/app/component/modules/blog/blogMain';
+// import BlogPage from '@/app/component/modules/blog/blogMain';
 import blogData from '@/app/component/data/newsroom/newsroomData.json';
 import styles from '@/styles/blog/blogMain.module.css'
 import NewsroomPage from '@/app/component/modules/newsroom/newroomMain';
